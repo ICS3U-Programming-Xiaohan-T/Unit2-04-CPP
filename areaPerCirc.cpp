ce65413ec9a522bf7352161ee8205af0df253faa
Xiaohan Tian <xiaohan.tian@stu.ocsb.ca>
2025-03-07 02:56:07 +0000

    areaPerCircUpdated.cpp

--- areaPerCirc.cpp
+++ areaPerCirc.cpp
@@ -7,12 +7,17 @@
 #include <cmath>
 #include <iostream>
 int main() {
+    // declare the variables
     float radius, area, circumference;
+    // asking the radius from users
     std::cout << "Enter the radius of the circle (cm): ";
+    // store the data under the variable of radius
     std::cin >> radius;
+    // use following equation to process
     area = M_PI * pow(radius , 2);
     circumference = 2 * M_PI * radius;
     std::cout << "\n";
+    // output the area and circumference
     std::cout << "The area of the circle is: "
     << area << "cm^2" << std::endl;
     std::cout << "The circumference of the circle is: "
